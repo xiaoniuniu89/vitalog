d4c4e245a5f0a579da3f7b0b3c505edbf6c19a5f
Daniel Callaghan <dan_cal89@hotmail.com>
2024-02-02 12:47:21 +0000

    update

--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,16 @@ export default authMiddleware({
   apiRoutes: ["/(api|trpc)(.*)"],
   publicRoutes: ["/sign-in", "/sign-up", "/", "/api/webhooks(.*)"],
   afterAuth(auth, req, evt) {
-    // Allow access to all routes, including the webhook route
+    // Allow matched api/public requests
+    // If the user is logged in and trying to access a protected route, allow them to access route
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+    // Allow users visiting public routes to access them
     return NextResponse.next();
   },
 });
 
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/",
-    "/dashboard",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
