d2e8ab78b53813fa01cac79491343ba994d7f68e
Daniel Callaghan <dan_cal89@hotmail.com>
2024-02-02 12:44:11 +0000

    update

--- middleware.ts
+++ middleware.ts
@@ -11,5 +11,10 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/dashboard",
+    "/(api|trpc)(.*)",
+  ],
 };
